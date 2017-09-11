c5831c0c91e55f790e9d642ca5f2d8e77e3174ef
Tiago Cordeiro <tiago@mulhergorila.com>
2017-09-11 13:05:50 -0300

    Create pydistmail.py

---
+++ pydistmail.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# coding: utf-8
+
+import email
+import smtplib
+import sys
+
+# Captura mensagem do stdin via pipe
+msg = email.message_from_file(sys.stdin)
+
+# lista com vendedores ou emails para distribuição
+vendedores = [('Vendedor 01', 'vendas01@dominio.com.br'),
+              ('Vendedor 02', 'vendas02@dominio.com.br')]
+
+# Verifica o tamanho da lista de vendedores
+totalVendedores = len(vendedores)
+
+# Pega o ultimo vendedor que recebeu email
+ultimovendedor = open('ultimovendedor.txt', 'r').read()  # use o path completo
+
+# Determina o próximo vendedor que irá receber e-mail
+if int(ultimovendedor) == totalVendedores - 1:  # Se o ultimo que recebeu, é o
+    proximovendedor = 0  # ultimo da lista, volta para
+else:  # o primeiro da lista.
+    proximovendedor = int(ultimovendedor) + 1  # Se não, pega o próximo.
+
+davez = str(proximovendedor)
+
+# Salva qual vendedor está recebendo o e-mail atual.
+fvend = open('ultimovendedor.txt', 'w')  # use o path completo
+fvend.write(davez)
+fvend.close()
+
+# Encaminha o e-mail para o vendedor da vez.
+s = smtplib.SMTP('localhost')
+s.sendmail("vendas@dominio.com.br", vendedores[proximovendedor][1],
+           msg.as_string())
+s.quit()
